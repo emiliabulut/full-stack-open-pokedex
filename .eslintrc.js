497dae4313154e7d21f8d33d3592a31978117c54
Emilia Bulut <emilia.bulut@tuni.fi>
2024-04-08 22:30:22 +0300

    fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   'env': {
     'node': true,
     'es6': true,
@@ -42,6 +42,56 @@ module.exports = {
     ],
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 'error',
+    'react/prop-types': 0
+  }
+}
+*/
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    'jest/globals': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react', 'jest'
+  ],
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'never'
+    ],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { before: true, after: true }
     ],
     'no-console': 'error',
     'react/prop-types': 0
